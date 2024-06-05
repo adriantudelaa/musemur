f5c9f8f16222af12f38555813a8f1ae297f899bf
adriantudela <1762891@alu.murciaeduca.es>
2024-06-05 14:09:03 +0200

    fix

--- src/routes/exposiciones.routes.js
+++ src/routes/exposiciones.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { getAllExposiciones, getExposicionesByMuseum, addExposicion, editExposicion, deleteExposicion } from "../controllers/exposiciones.controllers.js";
+import { getAllExposiciones, getExposicionesByMuseum, addExposicion, editExposicion, deleteExposicion } from "../controllers/exposiciones.controller";
 
 const router = Router();
 
 router.get("/exposiciones", getAllExposiciones);
+
 router.post("/exposiciones/museum", getExposicionesByMuseum);
+
 router.post("/exposiciones", addExposicion);
+
 router.put("/exposiciones", editExposicion);
+
 router.delete("/exposiciones", deleteExposicion);
 
 export default router;
