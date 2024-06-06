991869dd1259e85b84a413fcedc567b1b9f2a3a5
adriantudela <1762891@alu.murciaeduca.es>
2024-06-06 21:55:32 +0200

    Refactor app.js to remove unused route for administrators

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import express from 'express';
 import usuariosRoutes from './routes/usuarios.routes.js';
 import museosRoutes from './routes/museos.routes.js';
 import reservasRoutes from './routes/reservas.routes.js';
-import administradoresRoutes from './routes/administradores.routes.js';
 import chatboxRoutes from './routes/chatbox.routes.js';
 import exposicionesRoutes from './routes/exposiciones.routes.js';
 
