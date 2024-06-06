f8b8ec77ff42ffff6f5ad5de745a2b9e1f7f6afb
adriantudela <1762891@alu.murciaeduca.es>
2024-06-06 18:37:56 +0200

    clean code

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { seeUsers, createUser, updateUserData, updateUserPswrd, deleteUser, loginUser, loginAdmin, resetPassword, verifyAdminDni, getUserProfile, getAdminProfile } from "../controllers/usuarios.controller.js";
+import { createUser, updateUserData, updateUserPswrd, deleteUser, loginUser, loginAdmin, resetPassword, getUserProfile } from "../controllers/usuarios.controller.js";
 import { verifyToken } from '../../middlewares/auth.js';
 
 const router = Router();
 
-router.get("/User", verifyToken, seeUsers);
 router.post("/User", createUser);
 router.put("/UserData", verifyToken, updateUserData);
 router.put("/UserPswrd", verifyToken, updateUserPswrd);
@@ -12,8 +11,6 @@ router.delete("/User", verifyToken, deleteUser);
 router.post('/Userlogin', loginUser);
 router.post('/AdminLogin', loginAdmin);
 router.post('/ResetPassword', resetPassword);
-router.post('/VerifyAdminDni', verifyAdminDni);
 router.get('/UserProfile', verifyToken, getUserProfile);
-router.get('/AdminProfile', verifyToken, getAdminProfile);
 
 export default router;
