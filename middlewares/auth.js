60d225cc93a855da99f7a955119dd8caed4ecd03
adriantudela <1762891@alu.murciaeduca.es>
2024-06-06 04:16:06 +0200

    middleware actualizado

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,17 +7,17 @@ const { JWT_SECRET } = process.env;
 
 export const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
-    
+
     if (!token) {
-        return res.status(403).json({ message: 'Token no proporcionado' });
+        return res.status(403).json({ message: 'No token provided' });
     }
 
-    try {
-        const decoded = jwt.verify(token, JWT_SECRET);
+    jwt.verify(token.split(' ')[1], JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(500).json({ message: 'Failed to authenticate token' });
+        }
+
         req.userId = decoded.id;
-        req.userRole = decoded.role;
         next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Token inválido' });
-    }
+    });
 };
